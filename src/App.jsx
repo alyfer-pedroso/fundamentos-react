507daaeafa47c66a0fa979b9ab7b3420f54401ef
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-11-17 01:52:47 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Mega from "./components/mega/Mega";
+import Contador from "./components/contador/Contador";
 import Input from "./components/formulario/Input";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
 import DiretaPai from "./components/comunicacao/DiretaPai";
@@ -22,6 +24,14 @@ export default () => (
         <h1>Fundamentos React</h1>
 
         <div className="Cards-Container">
+            <Card titulo="#13 - Mega" color="#b9006e">
+                <Mega qtd={8}></Mega>
+            </Card>
+
+            <Card titulo="#12 - Contador" color="#424242">
+                <Contador numeroInicial={10}></Contador>
+            </Card>
+
             <Card titulo="#11 - Componente Controlado (Input)" color="#e45f56">
                 <Input></Input>
             </Card>
